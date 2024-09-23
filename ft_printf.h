5089b272c7ecd52cfdcee14445ba51c6cc4a3af7
akhamass <ayoubkhamassi@MacBook-Air-de-Ayoub.local>
2024-09-23 14:14:18 +0200

    Success with 100% grade

---
+++ ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akhamass <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/13 17:44:13 by akhamass          #+#    #+#             */
+/*   Updated: 2024/01/13 17:44:15 by akhamass         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "libft.h"
+# include <stdarg.h>
+# include <limits.h>
+# include <stdio.h>
+
+int		ft_printf(const char *__format, ...);
+int		prt_str(char const *str);
+int		prt_ptr(void *ptr_addr);
+int		prt_int(int n);
+int		prt_unsigned(unsigned int nbr);
+int		prt_hexa(unsigned int nbr, bool upper_case);
+
+#endif
